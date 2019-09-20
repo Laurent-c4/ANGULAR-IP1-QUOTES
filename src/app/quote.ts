8410c1b8b0108c111f4938a31729905d4fd60b8c
Laurent Juma <lcbillax.lj@gmail.com>
2019-09-20 17:34:49 +0300

    Add quote

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,5 +1,5 @@
 export class Quote {
-    constructor(public id:number, public quote:string, public upvotes:number, public downvotes:number, 
+    constructor(public id:number, public quoteText:string, public upvotes:number, public downvotes:number, 
         public author:string, public date:Date) {
     }
 }
